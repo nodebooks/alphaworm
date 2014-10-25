ca639ffa34c85344dbcbea86d2d2f62dd300631e
Jaakko Vanhala <nodebooks@gmail.com>
2014-10-25 16:42:14 +0300

    Fixed indentation

--- client/messagebroker.js
+++ client/messagebroker.js
@@ -1,46 +1,46 @@
 var MessageBroker = function(messageHandler) {
 
-    var self = this;
-    self.messageHandler = messageHandler;
+  var self = this;
+  self.messageHandler = messageHandler;
 
-    self.init = function() {
-        console.log("MessageBroker started");
+  self.init = function() {
+    console.log("MessageBroker started");
 
-        // The websocketURI we received from the server
-        console.log("websocketURI = ", websocketURI);
-        self.ws = new WebSocket('ws://' + websocketURI);
+    // The websocketURI we received from the server
+    console.log("websocketURI = ", websocketURI);
+    self.ws = new WebSocket('ws://' + websocketURI);
 
-        self.ws.onmessage = function (event) {
-            console.log("MSG:", JSON.parse(event.data));
-            self.receive(event.data);
-        };
+    self.ws.onmessage = function (event) {
+      console.log("MSG:", JSON.parse(event.data));
+      self.receive(event.data);
+    };
 
-        self.ws.onerror = function (event) {
-            console.log("websocket failed");
-            document.getElementById('system-messages').innerHTML += "<br />Websocket failed";
-        };
+    self.ws.onerror = function (event) {
+      console.log("websocket failed");
+      document.getElementById('system-messages').innerHTML += "<br />Websocket failed";
+    };
 
-        self.ws.onopen = function (event) {
-            console.log("websocket connected");
-            document.getElementById('system-messages').innerHTML += "<br />Websocket connected";
-        };
+    self.ws.onopen = function (event) {
+      console.log("websocket connected");
+      document.getElementById('system-messages').innerHTML += "<br />Websocket connected";
+    };
 
-        self.ws.onclose = function (event) {
-            console.log("websocket disconnected");
-            document.getElementById('system-messages').innerHTML += "<br />Websocket disconnected";
+    self.ws.onclose = function (event) {
+      console.log("websocket disconnected");
+      document.getElementById('system-messages').innerHTML += "<br />Websocket disconnected";
 
-        };
-    },
+    };
+  },
 
-    self.receive = function(msg) {
-        // Use JSON.parse() to deserialize the JavaScript object
-        self.messageHandler.receive(JSON.parse(msg));
-    },
+  self.receive = function(msg) {
+    // Use JSON.parse() to deserialize the JavaScript object
+    self.messageHandler.receive(JSON.parse(msg));
+  },
 
-    self.send = function(msg) {
-        // Use JSON.stringify() to serialize message to a JavaScript object
-        self.ws.send(JSON.stringify(msg));
-    }
+  self.send = function(msg) {
+    // Use JSON.stringify() to serialize message to a JavaScript object
+    self.ws.send(JSON.stringify(msg));
+  }
 
-    self.init();
+  self.init();
 }
--- client/messagehandler.js
+++ client/messagehandler.js
@@ -1,81 +1,81 @@
 var MessageHandler = function() {
 
-    var self = this;
-    self.messageBroker = undefined;
-    self.username = undefined;
+  var self = this;
+  self.messageBroker = undefined;
+  self.username = undefined;
 
-    self.init = function() {
-        console.log("MessageHandler started");
-        self.messageBroker = new MessageBroker(self);
-    },
+  self.init = function() {
+    console.log("MessageHandler started");
+    self.messageBroker = new MessageBroker(self);
+  },
 
-    self.attachBroker = function(messageBroker) {
-        console.log("MessageHandler: messageBroker attached.")
-        self.messageBroker = messageBroker;
-    },
+  self.attachBroker = function(messageBroker) {
+    console.log("MessageHandler: messageBroker attached.")
+    self.messageBroker = messageBroker;
+  },
 
-    self.send = function(msg) {
-        self.messageBroker.send(msg);
-    },
+  self.send = function(msg) {
+    self.messageBroker.send(msg);
+  },
 
-    self.receive = function(msg) {
-        switch(msg.name) {
+  self.receive = function(msg) {
+    switch(msg.name) {
 
-            case 'REGISTRATION_RESPONSE':
-                console.log("MessageHandler: REGISTRATION_RESPONSE", msg.status);
-            break;
+      case 'REGISTRATION_RESPONSE':
+      console.log("MessageHandler: REGISTRATION_RESPONSE", msg.status);
+      break;
 
-            case 'LOGIN_RESPONSE':
-                console.log("MessageHandler: LOGIN_RESPONSE", msg.status);
-                self.handleLoginResponse(msg);
-            break;
+      case 'LOGIN_RESPONSE':
+      console.log("MessageHandler: LOGIN_RESPONSE", msg.status);
+      self.handleLoginResponse(msg);
+      break;
 
-            default:
-                console.log("MessageHandler: default branch reached");
-            break;
-        }
-    },
-
-    self.handleLoginResponse = function(msg) {
-        if(msg.status == "OK" && msg.username) {
-            self.setUsername(msg.username);
-            document.getElementById('infotext').style.color = "black";
-            document.getElementById('infotext').innerHTML = "Player <strong>" +
-            msg.username + "</strong> logged in.";
-            document.getElementById('infotext').innerHTML += '<br /><input id="logout_button" type="submit" value="Exit" onclick="logout();">';
-        }
-        else {
-            var tmp = document.getElementById('infotext').innerHTML;
-            //console.log(tmp);
-            document.getElementById('infotext').style.color = "red";
-            document.getElementById('infotext').innerHTML = "Login failed.";
-            var t = setTimeout(function() { document.getElementById('infotext').innerHTML = tmp; document.getElementById('infotext').style.color = "black"; }, 2000)
-        }
-    },
-
-    self.handleRegistrationResponse = function(msg) {
-        if(msg.status == "OK" && msg.username) {
-            self.setUsername(msg.username);
-            document.getElementById('infotext').style.color = "black";
-            document.getElementById('infotext').innerHTML = "New player <strong>" +
-            msg.username + "</strong> registered.";
-            document.getElementById('infotext').innerHTML += '&nbsp;&nbsp;<input id="logout_button" type="submit" value="Logout" onclick="logout();">';
-        }
-        else {
-            var tmp = document.getElementById('infotext').innerHTML;
-            document.getElementById('infotext').style.color = "red";
-            document.getElementById('infotext').innerHTML = "Login failed.";
-            var t = setTimeout(function() { document.getElementById('infotext').innerHTML = tmp; document.getElementById('infotext').style.color = "black"; }, 2000)
-        }
-    },
-
-    self.setUsername = function(username) {
-        self.username = username;
-    },
-
-    self.getUsername = function() {
-        return self.username;
+      default:
+      console.log("MessageHandler: default branch reached");
+      break;
     }
+  },
 
-    self.init();
+  self.handleLoginResponse = function(msg) {
+    if(msg.status == "OK" && msg.username) {
+      self.setUsername(msg.username);
+      document.getElementById('infotext').style.color = "black";
+      document.getElementById('infotext').innerHTML = "Player <strong>" +
+      msg.username + "</strong> logged in.";
+      document.getElementById('infotext').innerHTML += '<br /><input id="logout_button" type="submit" value="Exit" onclick="logout();">';
+    }
+    else {
+      var tmp = document.getElementById('infotext').innerHTML;
+        //console.log(tmp);
+        document.getElementById('infotext').style.color = "red";
+        document.getElementById('infotext').innerHTML = "Login failed.";
+        var t = setTimeout(function() { document.getElementById('infotext').innerHTML = tmp; document.getElementById('infotext').style.color = "black"; }, 2000)
+      }
+    },  
+
+  self.handleRegistrationResponse = function(msg) {
+    if(msg.status == "OK" && msg.username) {
+      self.setUsername(msg.username);
+      document.getElementById('infotext').style.color = "black";
+      document.getElementById('infotext').innerHTML = "New player <strong>" +
+      msg.username + "</strong> registered.";
+      document.getElementById('infotext').innerHTML += '&nbsp;&nbsp;<input id="logout_button" type="submit" value="Logout" onclick="logout();">';
+    }
+    else {
+      var tmp = document.getElementById('infotext').innerHTML;
+      document.getElementById('infotext').style.color = "red";
+      document.getElementById('infotext').innerHTML = "Login failed.";
+      var t = setTimeout(function() { document.getElementById('infotext').innerHTML = tmp; document.getElementById('infotext').style.color = "black"; }, 2000)
+    }
+  },
+
+  self.setUsername = function(username) {
+    self.username = username;
+  },
+
+  self.getUsername = function() {
+    return self.username;
+  }
+
+  self.init();
 }
