18a6a9a5b9e125a29b2ca05bde6fd11f5cd6795b
JVanhalen <nodebooks@gmail.com>
2014-11-21 19:18:42 +0200

    Code refactoring & minor fixes

--- database/postgresql.sql
+++ database/postgresql.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE mygamedb;
-\c mygamedatabase;
+CREATE DATABASE alphaworm;
+\c alphaworm;
 
 CREATE EXTENSION pgcrypto;
 CREATE OR REPLACE FUNCTION SHA1(bytea) returns text AS $$
@@ -7,10 +7,10 @@ CREATE OR REPLACE FUNCTION SHA1(bytea) returns text AS $$
 $$ LANGUAGE SQL STRICT IMMUTABLE;
 
 CREATE TABLE userdata (
-        username VARCHAR(12) PRIMARY KEY,
-        password_hash VARCHAR(40),
-        registered TIMESTAMP DEFAULT NOW()
-        );
+  username VARCHAR(12) PRIMARY KEY,
+  password_hash VARCHAR(40),
+  registered TIMESTAMP DEFAULT NOW()
+);
 
 INSERT INTO userdata (username, password_hash) VALUES ('jaakko', SHA1('myexcellentpassword'));
 INSERT INTO userdata (username, password_hash) VALUES ('anssi', SHA1('anotherexcellentpwd'));
