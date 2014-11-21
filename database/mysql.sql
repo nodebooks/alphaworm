aee616e440d9b15efc40427965c889ae7ab84731
JVanhalen <nodebooks@gmail.com>
2014-11-21 19:18:31 +0200

    Code refactoring & minor fixes

--- database/mysql.sql
+++ database/mysql.sql
@@ -4,8 +4,8 @@ USE alphaworm; -- Use the db we just created
 
 -- Create the table to store the user data
 CREATE TABLE userdata (
-        username VARCHAR(12) PRIMARY KEY NOT NULL,
-        password_hash VARCHAR(40) NOT NULL,
-        registered TIMESTAMP DEFAULT NOW()
-        );
+  username VARCHAR(12) PRIMARY KEY NOT NULL,
+  password_hash VARCHAR(40) NOT NULL,
+  registered TIMESTAMP DEFAULT NOW()
+);
 
